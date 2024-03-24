741aa4b68014e35468b8f29bc098dd2597424ecb
Liam Tarpey <liam.m.tarpey@gmail.com>
2024-03-24 18:16:07 +0000

    build

--- app/components/seo/job-openings/JobOpenings.tsx
+++ app/components/seo/job-openings/JobOpenings.tsx
@@ -4,21 +4,27 @@ import { Flex } from '../../uikit';
 
 const CURRENT_JOBS = [
     {
+        name: 'Platform Engineer',
+        company: 'Hirewire',
+        location: 'Darmstadt, Germany',
+        url: 'https://hirewire.breezy.hr/p/26c20456d1a101-platform-developer-devops',
+    },
+    {
         name: 'Mobile Engineer - iOS',
         company: 'Hirewire',
-        location: 'Los Angeles, CA',
+        location: 'Los Angeles, CA, USA',
         url: 'https://hirewire.breezy.hr/p/e195f6a3fe7101-mobile-engineer-ios',
     },
     {
         name: 'Senior IT Systems Architect',
         company: 'Hirewire',
-        location: 'Melville, NY (Hybrid)',
+        location: 'Melville, NY, USA (Hybrid)',
         url: 'https://hirewire.breezy.hr/p/72ff5c3dd29b01-senior-it-systems-architect',
     },
     {
         name: 'Android Developer',
         company: 'Hirewire',
-        location: 'Los Angeles, CA',
+        location: 'Los Angeles, CA, USA',
         url: 'https://hirewire.breezy.hr/p/ed6d5c681d2201-android-developer',
     },
 ];
