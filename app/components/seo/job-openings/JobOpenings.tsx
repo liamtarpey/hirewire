4ca2b54417436e2d16a5996bac9b5c07056676f7
Liam Tarpey <liam.m.tarpey@gmail.com>
2024-03-24 18:13:46 +0000

    build

--- app/components/seo/job-openings/JobOpenings.tsx
+++ app/components/seo/job-openings/JobOpenings.tsx
@@ -7,19 +7,19 @@ const CURRENT_JOBS = [
         name: 'Mobile Engineer - iOS',
         company: 'Hirewire',
         location: 'Los Angeles, CA',
-        url: 'https://www.linkedin.com/jobs/search/?currentJobId=3869873164&f_C=98129050&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=3869873164%2C3869844007%2C3869872702&refId=0H17AlzWNyqNHpG%2FHNQkZg%3D%3D&trackingId=0roSvoadZt5hAsoUrwWGjw%3D%3D&trk=d_flagship3_company',
+        url: 'https://hirewire.breezy.hr/p/e195f6a3fe7101-mobile-engineer-ios',
     },
     {
         name: 'Senior IT Systems Architect',
         company: 'Hirewire',
         location: 'Melville, NY (Hybrid)',
-        url: 'https://www.linkedin.com/jobs/search/?currentJobId=3869844007&f_C=98129050&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=3869873164%2C3869844007%2C3869872702',
+        url: 'https://hirewire.breezy.hr/p/72ff5c3dd29b01-senior-it-systems-architect',
     },
     {
         name: 'Android Developer',
         company: 'Hirewire',
         location: 'Los Angeles, CA',
-        url: 'https://www.linkedin.com/jobs/search/?currentJobId=3869872702&f_C=98129050&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=3869873164%2C3869844007%2C3869872702',
+        url: 'https://hirewire.breezy.hr/p/ed6d5c681d2201-android-developer',
     },
 ];
 
