b593b895cf7da0f1074fb0131faf0863253795b9
Liam Tarpey <liam.m.tarpey@gmail.com>
2024-04-01 17:50:21 +0100

    build

--- app/components/seo/job-openings/JobOpenings.tsx
+++ app/components/seo/job-openings/JobOpenings.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import { PageWrap } from '../../uikit';
 import { StyledJobOpenings } from './JobOpenings.styled';
 import Script from 'next/script';
 
 export default function JobOpenings() {
+    const getUrl = () => {
+        const RANDOM_STRING = (Math.random() * 999).toString();
+        return `https://hirewire.breezy.hr/embed/js?inline=true&group_by=none&hirewire_v=${RANDOM_STRING}`;
+    };
+
     return (
         <>
-            <Script src="https://hirewire.breezy.hr/embed/js?inline=true&group_by=none"></Script>
+            <Script src={getUrl()} type="module"></Script>
             <PageWrap $limit="md">
                 <h2>Latest Job Openings</h2>
                 <StyledJobOpenings
