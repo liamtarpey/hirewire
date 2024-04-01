68ae7aa910ac1847831c06b2612e81b6d66f03fb
Liam Tarpey <liam.m.tarpey@gmail.com>
2024-04-01 18:01:36 +0100

    fix

--- app/components/seo/job-openings/JobOpenings.tsx
+++ app/components/seo/job-openings/JobOpenings.tsx
@@ -5,9 +5,13 @@ import { StyledJobOpenings } from './JobOpenings.styled';
 import Script from 'next/script';
 
 export default function JobOpenings() {
+    const getUrl = () => {
+        return `https://hirewire.breezy.hr/embed/js?inline=true&group_by=none&v=${Math.random().toString()}`;
+    };
+
     return (
         <>
-            <Script src="https://hirewire.breezy.hr/embed/js?inline=true&group_by=none"></Script>
+            <Script src={getUrl()}></Script>
             <PageWrap $limit="md">
                 <h2>Latest Job Openings</h2>
                 <StyledJobOpenings
