0cd8cf3ea07babf3a7ad067c60d34db98a7a2658
Liam Tarpey <liam.m.tarpey@gmail.com>
2024-04-01 19:36:53 +0100

    fix

--- app/components/seo/header/SEOHeader.tsx
+++ app/components/seo/header/SEOHeader.tsx
@@ -12,7 +12,6 @@ export default function SEOHeader() {
             <PageWrap $limit="md">
                 <StyledHeaderText>
                     <h1>Seamless Tech-Talent Pairing.</h1>
-
                     <p>Customized Placements, Tailored for Success</p>
                 </StyledHeaderText>
             </PageWrap>
