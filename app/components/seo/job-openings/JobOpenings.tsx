c1041eaf441ee63bc52f7f68d468cf18b5740ab3
Liam Tarpey <liam.m.tarpey@gmail.com>
2024-04-01 16:06:35 +0100

    build

--- app/components/seo/job-openings/JobOpenings.tsx
+++ app/components/seo/job-openings/JobOpenings.tsx
@@ -12,22 +12,28 @@ const CURRENT_JOBS = [
         url: 'https://hirewire.breezy.hr/p/12da327efdc101-senior-os-engineer',
     },
     {
-        name: 'Platform Engineer',
-        company: 'Hirewire',
-        location: 'Darmstadt, Germany',
-        url: 'https://hirewire.breezy.hr/p/26c20456d1a101-platform-developer-devops',
-    },
-    {
         name: 'Senior IT Systems Architect',
         company: 'Hirewire',
         location: 'Melville, NY (Hybrid)',
         url: 'https://hirewire.breezy.hr/p/72ff5c3dd29b01-senior-it-systems-architect',
     },
     {
+        name: 'Platform Engineer',
+        company: 'Hirewire',
+        location: 'Darmstadt, Germany',
+        url: 'https://hirewire.breezy.hr/p/26c20456d1a101-platform-developer-devops',
+    },
+    {
         name: 'System Engineer',
         company: 'Hirewire',
         location: 'Munich, Germany',
         url: 'https://hirewire.breezy.hr/p/966a5c0370b401-system-engineer',
+    },
+    {
+        name: 'Mobile App Developer (Capacitor)',
+        company: 'Hirewire',
+        location: 'Miami, FL',
+        url: 'https://hirewire.breezy.hr/p/369c281de47601-mobile-app-developer-capacitor',
     },
     {
         name: 'Freelance Recruiter / Sourcer',
